c9f1f1e82528f7ba624ad938e82f5339add189e4
Brendan <brendan.r.le@gmail.com>
2023-10-07 19:13:33 -0500

    added additional sections and js code

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+var saveInput = $('.saveBtn');
+
+saveInput.on('click', function(event) {
+  var target = $(event.target);
+  var item = target.siblings('.hour').text();
+
+  localStorage.setItem("scheduleSave" + item, target.siblings().eq(1).val());
+
+  $('#Display').append('Schedule item has been saved.');
+  setTimeout(remove, 3000);
+  
 });
+
+function renderSaves() {
+  for (var y = 9; y <= 11; y++) {
+    var info = localStorage.getItem('scheduleSave' + y +'AM')
+    $('#hour-' + y).children().eq(1).append(info);
+  };
+  var hour = 12
+  for (var g = 1; g <= 12; g++) {
+    var text = localStorage.getItem('scheduleSave' + g +'PM')
+    $('#hour-' + hour).children().eq(1).append(text);
+    hour++;
+  };
+};
+
+function remove(){
+  $('#Display').text('')
+}
+
+function displaytime(){
+  var datetime = dayjs().format('MMM D, YYYY, hh:mm:ss a');
+  $('#currentDay').text(datetime);
+};
+
+var i = 8;
+
+function editboxes(){
+  var time = dayjs().format('H');
+  Number(time);
+  
+  if (time > i) {
+    $('#hour-' + [i - 1]).removeClass('future present');
+    $('#hour-' + [i - 1]).addClass('past')
+    $('#hour-' + [i]).removeClass('future');
+    $('#hour-' + [i]).addClass('past');
+    i++;
+  } else {
+    $('#hour-' + [i]).removeClass('future past');
+    $('#hour-' + [i]).addClass('present');
+  }
+
+  if (i > 23) {
+    i = 8;
+    $('.time-block').removeClass('past present')
+    $('.time-block').addClass('future')
+  }
+}
+
+setInterval(editboxes, 500);
+editboxes();
+
+setInterval(displaytime, 1000);
+displaytime();
+
+renderSaves();
